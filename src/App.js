b16b30d5cf94e68933031fde06843ecd1c07c75f
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-04-16 17:09:05 -0600

    what i could figure out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+import './App.css' 
+import Character from './components/Character'
+import axios from 'axios';
+import {handlers} from './mocks/handlers'
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [currentCharacterId, setCurrentCharacterId] = useState(null)
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const openDetails = (id) => {
+    setCurrentCharacterId(id);
+  };
+
+  const closeDetails = () => {
+    setCurrentCharacterId(null);
+  }
+
+useEffect(() => {
+  axios.get(`${handlers}`)
+    .then(res => {
+      console.log(res);
+      setCharacters(res.data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}, [])
+
+const AddCharacter = (props) => (
+  <div className='character'>
+    {props.info.name}
+    <button onClick={() => openDetails(props.info.id)}>See details</button>
+  </div>
+);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -12,6 +43,14 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+      characters.map((ch) => {
+        return <AddCharacter key={ch.id} info={ch} />
+      })
+    }
+    {currentCharacterId && (
+      <Character characterId={currentCharacterId} close={closeDetails}/>
+    )} 
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+
+import React, { useState, useEffect} from 'react' 
+import {handlers} from '../mocks/handlers'
+import axios from 'axios'
+import App from '../App.js'
+
+ function Character(props) {
+    const { characterId, close } = props;
+    const [character, setCharacter] = useState(null)
+
+useEffect(() => {
+    axios.get(`${handlers}`)
+    .then((res) => {
+        setCharacter(res.data);
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}, [characterId])
+
+
+    return (
+        <div className='container'>
+            <h2>Details (of character with id {characterId}):</h2>
+            {character && (
+                <>
+                <p>
+                    {character.name}
+                </p>
+                </>
+            )}
+            <button onClick={close}>Close</button>
+        </div>
+    )
+}
+
+export default Character
